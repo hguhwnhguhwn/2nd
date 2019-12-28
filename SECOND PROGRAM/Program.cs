e31fd797b8171d0064ecc41cf8bd8d5854cc38bc
hglrwv <manyear_rtg2019@yahoo.com>
2019-12-28 18:37:11 +0200

    3 times

--- SECOND PROGRAM/Program.cs
+++ SECOND PROGRAM/Program.cs
@@ -11,6 +11,7 @@ namespace SECOND_PROGRAM
         static void Main(string[] args)
         {
            // this is the second times we try to write a program 
+
             Console.WriteLine("THIS IS OUR TWON");
             Console.ReadKey();
         }
